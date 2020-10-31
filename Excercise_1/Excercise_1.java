f70f8a875d48b4670b6a496e20d9c112b3eb3d59
arsal-subhan <musu87774@th-wildau.de>
2020-10-31 18:52:49 +0100

    first commit

---
+++ Excercise_1/Excercise_1.java
@@ -0,0 +1,54 @@
+
+public class Excercise_1 
+{
+	
+	
+	// Method 
+	static int maxPrefix(String S , String T)
+ { 
+		
+int i ;
+int j = 0;
+
+
+ for ( i = 1; i < S.length() && j < T.length(); i++) 
+ 
+   {
+    if(T.charAt(j)!=S.charAt(i)) 
+        {
+    	
+    	
+    	}
+    	
+  
+     else if (T.charAt(j)==S.charAt(i) ) 
+     
+           {
+    	
+    	break;
+    
+    	
+            }
+    
+    
+   }
+return i +1 ;
+
+ }
+    
+   
+
+	
+// Driver Code 
+public static void main(String args[]) 
+
+   { 
+	
+	String S = "abracadabra"; 
+	String T = "da"; 
+	
+System.out.println(maxPrefix(S,T)); 
+
+
+   } 
+} 
